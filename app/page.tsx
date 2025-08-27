bff9f4af2fff8d376203a7c8f23b2d8e98dae805
trwscholar <trwscholaryt@gmail.com>
2025-08-27 23:06:55 +0800

    Fix SSR window undefined error

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@ import { HeroSplit } from '@/components/hero-split';
 import { ValueProps } from '@/components/value-props';
 import { ServicesPreview } from '@/components/services-preview';
 import { HowItWorks } from '@/components/how-it-works';
-import { IndustriesServed } from '@/components/industries-served';
-import { StatsSection } from '@/components/stats-section';
 import { TestimonialSlider } from '@/components/testimonial-slider';
 import { LatestTips } from '@/components/latest-tips';
 import { CTASection } from '@/components/cta-section';
+import dynamic from 'next/dynamic';
+
+const IndustriesServed = dynamic(() => import('@/components/industries-served').then(mod => ({ default: mod.IndustriesServed })), {
+  ssr: false
+});
+
+const StatsSection = dynamic(() => import('@/components/stats-section').then(mod => ({ default: mod.StatsSection })), {
+  ssr: false
+});
 
 export default function Home() {
   return (
